a782c1be68dba7237b5f0bc4e198658f91ee598b
Jeferson Furrier <jeferson_51@hotmail.com>
2023-10-12 14:48:38 -0300

    Adicionar arquivos de projeto.

---
+++ GRPC_Consumer/Controllers/CallGrpcController.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GRPC_Consumer.Protos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GRPC_Consumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallGrpcController : ControllerBase
+    {
+        [HttpGet("message")]
+        public IActionResult GetMessageFromGrpc(string message)
+        {
+            //No good practices here, just for educational purposes :D
+            using var channel = GrpcChannel.ForAddress("http://localhost:5267");
+            var client = new Examples.ExamplesClient(channel);
+            var reply = client.GetText(
+                              new RequestExample { Text = message });
+            return Ok(reply);
+        }
+    }
+}
---
+++ GRPC_Server/Services/ExamplesService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using GRPC_Server.Protos;
+
+namespace GRPC_Server.Services
+{
+    public class ExamplesService : Examples.ExamplesBase
+    {
+        public override Task<ResponseExample> GetText(RequestExample request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResponseExample() { Text = $"You said: {request.Text}, i say: Hellowwww"});
+        }
+    }
+}
